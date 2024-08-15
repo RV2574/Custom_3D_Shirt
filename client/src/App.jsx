ab72763ad4aff63ff758684cccc10518cf87bf6f
rhythm <120657452+RV2574@users.noreply.github.com>
2024-08-15 18:41:11 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,21 @@
+import Canvas from './canvas';
+import Customizer from './pages/Customizer';
+import Home from './pages/Home';
+
+
+import './App.css'
+
+function App() {
+  
+  return (
+   <main className="app transition-all 
+   ease-in">
+<Home/>
+<Canvas/>
+<Customizer/>
+
+   </main>
+  )
+}
+
+export default App
